0c4bfaed248df24ac42e05b90b60f0eaa4c15841
ZP <991475623@qq.com>
2022-02-10 22:40:12 +0800

    updata

--- java-juc/src/main/java/com/zp/ContainerNotSafeDemo.java
+++ java-juc/src/main/java/com/zp/ContainerNotSafeDemo.java
@@ -1,8 +1,9 @@
 package com.zp;
 
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CopyOnWriteArraySet;
 
 /**
  * @author ZP
@@ -36,12 +37,22 @@ public class ContainerNotSafeDemo {
      */
     public static void main(String[] args) {
         List<String> list = new CopyOnWriteArrayList<>();
+//        for (int i = 1; i <= 30; i++) {
+//            new Thread(() -> {
+//                list.add(UUID.randomUUID().toString().substring(1, 8));
+//                System.out.println(list);
+//            }, String.valueOf(i)).start();
+//        }
+
+        Set<String> set = new CopyOnWriteArraySet<>();
         for (int i = 1; i <= 30; i++) {
             new Thread(() -> {
-                list.add(UUID.randomUUID().toString().substring(1, 8));
-                System.out.println(list);
+                set.add(UUID.randomUUID().toString().substring(1, 8));
+                System.out.println(set);
             }, String.valueOf(i)).start();
         }
+
+        Map<String, String> hashMap = new ConcurrentHashMap<String, String>();
         /**
          * 1.故障现象
          *  java.util.ConcurrentModificationException
